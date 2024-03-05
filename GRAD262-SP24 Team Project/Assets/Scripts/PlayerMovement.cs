a638bd7d9776704d878c1b59449b4b37381c59f8
John Sissler <jsissler@gmail.com>
2024-03-05 17:35:20 -0500

    Refactoring of PlayerMovement.cs
    Got rid of clamp rotation notion. Added a bit of noise.

--- GRAD262-SP24 Team Project/Assets/Scripts/PlayerMovement.cs
+++ GRAD262-SP24 Team Project/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float forwardThrustPower = 1000f;
-    public float yawSpeed = 1000f;
-    public float pitchSpeed = 1000f;
-    public float rollSpeed = 1000f;
-    public float deadZoneRadius = 0.15f;
-    public bool clampRotation = true;
-    public Vector3 rotationMin = new Vector3(-10, -10, -10);
-    public Vector3 rotationMax = new Vector3(10, 10, 10);
+    [Header("Thrust, Yaw, Pitch, & Roll Force")]
+    public float thrustForce = 10000f;
+    public float maximumVelocityMagnitude = 1000f;
+    public float yawTorque = 1000f;
+    public float pitchTorque = -1000f;
+    public float rollTorque = 1000f;
+
+    [Header("Roll Noise")]
+    public float rollNoise = 1f;
+    public float rollNoiseInterval = 2.5f;
+    public float lastRollNoise = 0;
+
+    [Header("Yaw Noise")]
+    public float yawNoise = 1f;
+    public float yawNoiseInterval = 5f;
+    public float lastYawNoise = 0;
+
+    [Header("Pitch Noise")]
+    public float pitchNoise = 1f;
+    public float pitchNoiseInterval = 3f;
+    public float lastPitchNoise = 0;
+
+    [Header("Mouse Deadzone; Zero-velocity Easing")]
+    public float deadZoneRadius = 0.5f;
+    public float easing = 0.01f;
+
+    [Header("Type 'p' to toggle pause")]
+    public bool paused = false;
 
     private Vector3 _centerOfScreen;
     private Rigidbody _rigidBody;
+    private bool _rollNoiseStart = true;
+    private bool _yawNoiseStart = true;
+    private bool _pitchNoiseStart = true;
 
     private void Start()
     {
@@ -22,30 +45,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        GetKeyboardInputs();
-        GetMouseInputs();
+        if (Input.GetKeyDown(KeyCode.P))
+            paused = !paused;
 
-        if (clampRotation)
-            ClampRotation();
-    }
-
-    private float ClampAngle(float angle, float from, float to)
-    {
-        if (angle < 0f)
-            angle = 360 + angle;
-
-        if (angle > 180f)
-            return Mathf.Max(angle, 360 + from);
-
-        return Mathf.Min(angle, to);
-    }
-
-    private void ClampRotation()
-    {
-        transform.rotation = Quaternion.Euler(new Vector3(
-            ClampAngle(transform.rotation.eulerAngles.x, rotationMin.x, rotationMax.x),
-            ClampAngle(transform.rotation.eulerAngles.y, rotationMin.y, rotationMax.y),
-            ClampAngle(transform.rotation.eulerAngles.z, rotationMin.x, rotationMax.z)));
+        if (!paused)
+        {
+            GetKeyboardInputs();
+            GetMouseInputs();
+        }
     }
 
     private void GetKeyboardInputs()
@@ -64,7 +71,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void ForwardThrust(float thrust)
     {
-        _rigidBody.AddForce(gameObject.transform.forward * thrust * forwardThrustPower * Time.deltaTime);
+        Thrust(gameObject.transform.forward * thrust * thrustForce);
     }
 
     private void YawMovement(float yaw)
@@ -72,7 +79,25 @@ public class PlayerMovement : MonoBehaviour
         if (Mathf.Abs(yaw) < deadZoneRadius)
             yaw = 0;
 
-        _rigidBody.AddTorque(gameObject.transform.up * yaw * yawSpeed * Time.fixedDeltaTime);
+        if (yaw != 0)
+        {
+            Rotate(gameObject.transform.up * yaw * yawTorque);
+            lastYawNoise = Time.time;
+        }
+        else if (Time.time > lastYawNoise + yawNoiseInterval)
+        {
+            if (_yawNoiseStart)
+            {
+                _yawNoiseStart = false;
+
+                if (Random.Range(0, 1) < .5)
+                    yawNoise = -yawNoise;
+            }
+
+            Rotate(gameObject.transform.up * yawNoise * yawTorque);
+            yawNoise = -yawNoise;
+            lastYawNoise = Time.time;
+        }
     }
 
     private void PitchMovement(float pitch)
@@ -80,12 +105,64 @@ public class PlayerMovement : MonoBehaviour
         if (Mathf.Abs(pitch) < deadZoneRadius)
             pitch = 0;
 
-        _rigidBody.AddTorque(gameObject.transform.right * pitch * pitchSpeed * Time.fixedDeltaTime);
+        if (pitch != 0)
+        {
+            Rotate(gameObject.transform.right * pitch * pitchTorque);
+            lastPitchNoise = Time.time;
+        }
+        else if (Time.time > lastPitchNoise + pitchNoiseInterval)
+        {
+            if (_pitchNoiseStart)
+            {
+                _pitchNoiseStart = false;
+
+                if (Random.Range(0, 1) < .5)
+                    pitchNoise = -pitchNoise;
+            }
+
+            Rotate(gameObject.transform.right * pitchNoise * pitchTorque);
+            pitchNoise = -pitchNoise;
+            lastPitchNoise = Time.time;
+        }
     }
 
     private void RollMovement(float roll)
     {
-        _rigidBody.AddTorque(gameObject.transform.forward * roll * rollSpeed * Time.fixedDeltaTime);
+        if (roll != 0)
+        {
+            Rotate(gameObject.transform.forward * roll * rollTorque);
+            lastRollNoise = Time.time;
+        }
+        else if (Time.time > lastRollNoise + rollNoiseInterval)
+        {
+            if (_rollNoiseStart)
+            {
+                _rollNoiseStart = false;
+
+                if (Random.Range(0, 1) < .5)
+                    rollNoise = -rollNoise;
+            }
+            Rotate(gameObject.transform.forward * rollNoise * rollTorque);
+            rollNoise = -rollNoise;
+            lastRollNoise = Time.time;
+        }
+    }
+
+    private void Thrust(Vector3 vector)
+    {
+        if (!Mathf.Approximately(0f, vector.magnitude))
+        {
+            if (_rigidBody.velocity.magnitude < maximumVelocityMagnitude)
+                _rigidBody.AddForce(vector * Time.fixedDeltaTime);
+        }
+        else
+            _rigidBody.velocity = Vector3.Lerp(_rigidBody.velocity, Vector3.zero, easing);
+    }
+
+    private void Rotate(Vector3 vector)
+    {
+        if (!Mathf.Approximately(0f, vector.magnitude))
+            _rigidBody.AddTorque(vector * Time.fixedDeltaTime);
     }
 }
 
