e3b1247b6c38d6101c837bf61e2a0da4377c14f8
John  Sissler <34193125+jsissler@users.noreply.github.com>
2024-04-04 15:31:34 -0400

    Balanced sliders and value transfer

--- GRAD262-SP24 Team Project/Assets/Scripts/UIManager.cs
+++ GRAD262-SP24 Team Project/Assets/Scripts/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -50,16 +51,38 @@ public class UIManager : Singleton<UIManager>
     private void ElementLasered(Laserable.LaserableElements element, float amount)
     {
         Debug.Log($"lasered element {element} amt {amount}");
+
+        if (element == Laserable.LaserableElements.Gold)
+        {
+            Gold.value += amount;
+        }
     }
 
     public void PayForResource(string element)
     {
         Debug.Log($"PayForResource element {element}");
+
+        Laserable.LaserableElements enumElement;
+
+        Enum.TryParse<Laserable.LaserableElements>(element, out enumElement);
+
+        if (enumElement == Laserable.LaserableElements.Gold && Gold.value > 0)
+        {
+            transferValue(Gold, playerWeapons);
+            transferValue(Gold, playerLaser);
+        }
     }
 
     private void LaserFiring()
     {
         playerLaser.value -= laserDebitWhenFiring;
+    }
+
+    void transferValue(Slider source, Slider destination)
+    {
+        float transferAmt = Mathf.Min(source.value, destination.maxValue - destination.value);
+        source.value -= transferAmt;
+        destination.value += transferAmt;
     }
 
 }
