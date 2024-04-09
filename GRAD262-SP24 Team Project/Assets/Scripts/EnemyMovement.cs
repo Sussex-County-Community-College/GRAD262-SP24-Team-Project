b180fa949f05c08f2e7ad2decfa09fe51df5d957
John Sissler <jsissler@gmail.com>
2024-04-09 11:32:47 -0400

    Revised EnemyMovement enabling parallel moves

--- GRAD262-SP24 Team Project/Assets/Scripts/EnemyMovement.cs
+++ GRAD262-SP24 Team Project/Assets/Scripts/EnemyMovement.cs
@@ -1,71 +1,102 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : ShipMovement
 {
-    public enum MovementType { idle, thrust, roll, yaw, pitch, die };
+    public enum MovementType { idle, thrust, roll, yaw, pitch, restore, die };
 
     [System.Serializable]
     public struct Movement
     {
         public MovementType type;
         public float value;
+        public float startTime;
         public float duration;
     }
 
     [Header("Patrol Movement")]
     public Movement[] movements;
 
-    public int currentMovement = 0;
-    public float timeOfNextMovement = 0;
-
-    protected void Update()
-    {
-        if (Time.time > timeOfNextMovement)
-        {
-            currentMovement = (currentMovement + 1) % movements.Length;
-            timeOfNextMovement = Time.time + movements[currentMovement].duration;
-
-            if (movements[currentMovement].type == MovementType.die)
-                Destroy(gameObject);
-        }
-    }
+    private float _startTime = 0;
+    private Quaternion _startRotation;
+    private Dictionary<MovementType, Movement> _movementsThisFrame = new Dictionary<MovementType, Movement>();
 
     override protected void Start()
     {
         base.Start();
-        
-        if (movements.Length > 0)
+        _startTime = Time.time;
+        _startRotation = transform.rotation;
+    }
+
+    protected void Update()
+    {
+        _movementsThisFrame.Clear();
+
+        foreach (Movement movement in movements)
         {
-            currentMovement = 0;
-            timeOfNextMovement = Time.time + movements[currentMovement].duration;
+            Debug.Log($"type={ movement.type.ToString()}");
+
+            float movementStartTime = _startTime + movement.startTime;
+            float movementEndTime = movementStartTime + movement.duration;
+
+            if (Time.time > movementStartTime && Time.time < movementEndTime)
+            {
+                Debug.Log($"active type={ movement.type.ToString()}");
+
+                if (_movementsThisFrame.ContainsKey(movement.type))
+                    Debug.LogWarning($"ignoring key {movement.type} overlap, startTime={movement.startTime}");
+                else
+                {
+                    if (movement.type == MovementType.die)
+                    {
+                        Destroy(gameObject);
+                    }
+                    else if (movement.type == MovementType.restore)
+                    {
+                        GetComponent<Rigidbody>().isKinematic = true;
+                        transform.rotation = Quaternion.Slerp(transform.rotation, _startRotation, 0.5f);
+                    }
+                    else
+                    {
+                        GetComponent<Rigidbody>().isKinematic = false;
+                        _movementsThisFrame.Add(movement.type, movement);
+                    }
+                }
+            }
+
+            if (_movementsThisFrame.Count == 0)
+                _startTime = Time.time;
         }
+
+        
     }
 
     protected override float GetPitch()
     {
-        if (movements.Length > 0 && movements[currentMovement].type == MovementType.pitch)
-            return movements[currentMovement].value;
+        if (_movementsThisFrame.ContainsKey(MovementType.pitch))
+            return _movementsThisFrame[MovementType.pitch].value;
         return 0;
     }
 
     protected override float GetRoll()
     {
-        if (movements.Length > 0 && movements[currentMovement].type == MovementType.roll)
-            return movements[currentMovement].value;
+        if (_movementsThisFrame.ContainsKey(MovementType.roll))
+            return _movementsThisFrame[MovementType.roll].value;
         return 0;
     }
 
     protected override float GetThrust()
     {
-        if (movements.Length > 0 && movements[currentMovement].type == MovementType.thrust)
-            return movements[currentMovement].value;
+        if (_movementsThisFrame.ContainsKey(MovementType.thrust))
+            return _movementsThisFrame[MovementType.thrust].value;
         return 0;
     }
 
     protected override float GetYaw()
     {
-        if (movements.Length > 0 && movements[currentMovement].type == MovementType.yaw)
-            return movements[currentMovement].value;
+        if (_movementsThisFrame.ContainsKey(MovementType.yaw))
+            return _movementsThisFrame[MovementType.yaw].value;
         return 0;
     }
+
 }
