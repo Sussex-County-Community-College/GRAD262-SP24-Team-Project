e302836082d270d7406e472b43d76f97e5996c20
John Sissler <jsissler@gmail.com>
2024-04-18 12:02:48 -0400

    Added DontDestroyOnLoad

---
+++ GRAD262-SP24 Team Project/Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    public static SortedSet<string> instantiated = new SortedSet<string>();
+    public bool disableOnSceneChanged = false;
+
+    void Awake()
+    {
+        if (instantiated.Contains(name))
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instantiated.Add(name);
+            DontDestroyOnLoad(gameObject);
+            SceneManager.activeSceneChanged += OnSceneChanged;
+        }
+    }
+
+    private void OnSceneChanged(Scene current, Scene next)
+    {
+        if (disableOnSceneChanged)
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+    }
+}
