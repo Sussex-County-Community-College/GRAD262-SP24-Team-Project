bf9dd8b2ef7f327f1abb0f55858d6f076efdb7bc
John Sissler <jsissler@gmail.com>
2024-04-23 17:49:53 -0400

    updated everyone from master

--- GRAD262-SP24 Team Project/Assets/Scripts/MySceneManager.cs
+++ GRAD262-SP24 Team Project/Assets/Scripts/MySceneManager.cs
@@ -1,14 +1,38 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MySceneManager : Singleton<MySceneManager>
 {
+    private void Start()
+    {
+        DockingAssist dockingAssist = FindAnyObjectByType<DockingAssist>();
+
+        if (dockingAssist)
+        {
+            dockingAssist.onDockingStateChange.AddListener(OnDockingStateChange);
+        }
+    }
+
+    private void OnDockingStateChange(DockingAssist.DockingState dockingState)
+    {
+        if (dockingState == DockingAssist.DockingState.docked || dockingState == DockingAssist.DockingState.undocking)
+        {
+            ChangeScene();
+        }
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            int nextScene = (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings;
-            SceneManager.LoadScene(nextScene);
+            ChangeScene();
         }
+    }
+
+    private void ChangeScene()
+    {
+        int nextScene = (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextScene);
     }
 }
